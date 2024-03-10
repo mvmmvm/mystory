fbba2edd2cb0fc1c212540394168022718597487
Mai Sato <mu.mat.sig@gmail.com>
2024-03-11 03:19:04 +0900

    バックエンドとフロントエンドの独立化

--- test/controllers/characters_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CharactersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get characters_new_url
-    assert_response :success
-  end
-end
